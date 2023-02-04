924e80f170759711157615af0481b0201373450c
VampSlayer <sayam.hussain2008@gmail.com>
2023-02-04 16:20:58 +0000

    firebase hosting init

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,5 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <img width="100" height="100" src="https://i.imgur.com/xIzOjYI.png"/>
-  </header>
   <RouterView />
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoRjrHf5dD84tsIIdBllsd7LKnJFAh3B4",
+  authDomain: "civmoney.firebaseapp.com",
+  projectId: "civmoney",
+  storageBucket: "civmoney.appspot.com",
+  messagingSenderId: "110850086366",
+  appId: "1:110850086366:web:265b50a9946afae48b794f",
+  measurementId: "G-JHP3W77Z09"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase);
+
+// Initialize Vue
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +26,7 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(firebase);
+app.use(analytics);
 
 app.mount("#app");
